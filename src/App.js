fe52ab87b73c362cfe06ae66ad402d17b0b4fe22
Emre Boz <emrento@gmail.com>
2022-08-19 19:45:26 +0300

    usememo-ornek

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Todos from "./Todos";
 
+const cokZamanAlanFonksiyon = (num) => {
+  console.log("Hesaplama yapılıyor...");
+  for (let i = 0; i < 1000000000; i++) {
+    num += 1;
+  }
+  return num;
+};
 
 function App() {
   const [sayac, sayacGuncelle] = React.useState(0);
- 
   const [todos, setTodos] = React.useState([]);
+  const hesaplamaSonucu = React.useMemo(()=>cokZamanAlanFonksiyon(sayac),[todos]);
 
   const increment = () => {
     sayacGuncelle((oncekiSayac) => oncekiSayac + 1);
@@ -18,8 +25,11 @@ function App() {
     setTodos((oncekiDizi) => [...oncekiDizi, "Yapılacak iş"+saniye]);
   }, []);
 
+
+
   return (
     <>
+    <p>Hesaplama Sonucu: {hesaplamaSonucu}</p>
       <Todos todos={todos}  addTodo={addTodo}/> 
       <hr />
       <div>
