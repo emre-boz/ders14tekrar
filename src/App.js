b1d9816ef9c150b8427c93fea9798617745dc05e
Emre Boz <emrento@gmail.com>
2022-08-20 15:16:51 +0300

    usecontext kullanımı 5-6 aşama

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { useState } from "react";
 import C1 from './C1';
+import {OrtakBaglam1} from './contexts/OrtakBaglam1';
 
 
 
 
 function App() {
 const [isim,isimGuncelle]=useState("Ahmet Yılmaz");
+const [yas,yasGuncelle]=useState(35);
 
 
   return (
     <>
+    <OrtakBaglam1.Provider value={yas}>
       <C1 v1={isim}/>
+      </OrtakBaglam1.Provider>
     </>
   );
 }
 
 export default App; 
+
--- src/C4.js
+++ src/C4.js
@@ -1,7 +1,13 @@
+import { useContext } from "react";
+import {OrtakBaglam1} from './contexts/OrtakBaglam1';
+
 function C4({isim}){
+
+const yas=useContext(OrtakBaglam1);
+
     return(
         <>
-        <p>Ben C4 isim:{isim}</p>
+        <p>Ben C4 isim:{isim} Yaş:{yas}</p>
         </>
     )
 }
